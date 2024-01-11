9b628c741929dce7d08f1767d45be7ef97d772e2
Minjae-An <mj3242@naver.com>
2024-01-11 17:44:13 +0900

    chore : 놓친 프로젝트 초기 세팅 마저 구성

---
+++ src/main/java/com/example/springallinoneproject/SpringAllinoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springallinoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllinoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAllinoneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springallinoneproject/SpringAllinoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springallinoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllinoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
