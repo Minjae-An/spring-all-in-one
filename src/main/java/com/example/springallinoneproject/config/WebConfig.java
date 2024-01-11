16b00200cc443f4afa7b7fc01111eef1beff1eed
Minjae-An <mj3242@naver.com>
2024-01-11 17:57:04 +0900

    feat(WebConfig) : 구현한 커스텀 인터셉터 등록(#1)

---
+++ src/main/java/com/example/springallinoneproject/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.springallinoneproject.config;
+
+import com.example.springallinoneproject.interceptor.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/api/v1/user/**")
+                .addPathPatterns("/api/v1/manager/**");
+    }
+}
