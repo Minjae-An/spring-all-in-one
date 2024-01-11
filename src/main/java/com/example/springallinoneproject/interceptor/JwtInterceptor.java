e5535f52bb56b57ab26fc5dfa59d46a4a9494132
Minjae-An <mj3242@naver.com>
2024-01-11 17:56:11 +0900

    feat(JwtInterceptor) : JWT 검증 인터셉터 구현(#1)

---
+++ src/main/java/com/example/springallinoneproject/interceptor/JwtInterceptor.java
@@ -0,0 +1,35 @@
+package com.example.springallinoneproject.interceptor;
+
+import com.example.springallinoneproject.user.login.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtInterceptor implements HandlerInterceptor {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(!jwtUtil.isIncludeTokenPrefix(header)){
+            throw new IllegalStateException("No Bearer prefix");
+        }
+
+        String token = jwtUtil.extractTokenFromHeader(header);
+        if(jwtUtil.isTokenExpired(token)){
+            throw new IllegalStateException("token expired");
+        }
+
+        if(jwtUtil.isTokenNotManipulated(token)){
+            throw new IllegalStateException("token manipulated");
+        }
+
+        return true;
+    }
+}
