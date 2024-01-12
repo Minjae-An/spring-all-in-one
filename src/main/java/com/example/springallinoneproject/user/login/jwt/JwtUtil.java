d158d3b527f9fe983d3edc2952166a5cbac18355
Minjae-An <mj3242@naver.com>
2024-01-12 15:13:49 +0900

    feat(JwtUtil) :  login -> login.jwt 패키지로 이동(#2)

--- src/main/java/com/example/springallinoneproject/user/login/jwt/JwtUtil.java
+++ src/main/java/com/example/springallinoneproject/user/login/jwt/JwtUtil.java
@@ -1,7 +1,8 @@
-package com.example.springallinoneproject.user.login;
+package com.example.springallinoneproject.user.login.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.springallinoneproject.user.dto.LoggedInUser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,11 +11,13 @@ import java.util.Base64;
 import java.util.LinkedHashMap;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Slf4j
 public class JwtUtil {
     @Value("${jwt.secret-key}")
     private String secret;
@@ -22,40 +25,48 @@ public class JwtUtil {
     private String tokenPrefix = "Bearer ";
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    public boolean isIncludeTokenPrefix(String header) {
-        return header.split(" ")[0].equals(tokenPrefix.trim());
+    public boolean isIncludeTokenPrefix(String authorization) {
+        return authorization.split(" ")[0].equals(tokenPrefix.trim());
     }
 
 
-    public String extractTokenFromHeader(String header) {
-        return header.replace(tokenPrefix, "");
+    public String extractTokenFromAuthorization(String authorization) {
+        return authorization.replace(tokenPrefix, "");
     }
 
 
-    public String createToken(LoggedInUser loggedInUser, Instant currentDate) {
-        String token = JWT.create()
-                .withSubject(loggedInUser.getEmail())
-                .withExpiresAt(currentDate.plusMillis(expirationTimeMillis))
-                .withClaim("email", loggedInUser.getEmail())
-                .withClaim("username", loggedInUser.getUsername())
-                .sign(Algorithm.HMAC512(secret));
-
+    public String createAuthorization(LoggedInUser loggedInUser, Instant current) {
+        String token = createToken(loggedInUser, current);
         return tokenPrefix.concat(token);
     }
 
+    String createToken(LoggedInUser loggedInUser, Instant current) {
+        String token = JWT.create()
+                .withSubject(loggedInUser.getEmail())
+                .withExpiresAt(current.plusMillis(expirationTimeMillis))
+                .withClaim("email", loggedInUser.getEmail())
+                .withClaim("username", loggedInUser.getUsername())
+                .sign(Algorithm.HMAC512(secret));
+        return token;
+    }
+
     public boolean isTokenExpired(String token) {
-        Instant expiredAt = JWT.require(Algorithm.HMAC512(secret))
-                .build().verify(token)
+        Instant expiredAt = JWT.decode(token)
                 .getExpiresAtAsInstant();
 
         return expiredAt.isBefore(Instant.now());
     }
 
     public boolean isTokenNotManipulated(String token) {
-        return JWT.require(Algorithm.HMAC512(secret))
-                .build().verify(token)
-                .getSignature()
-                .equals(secret);
+        try{
+            JWT.require(Algorithm.HMAC512(secret))
+                    .build().verify(token)
+                    .getSignature();
+
+            return true;
+        }catch(JWTVerificationException e){
+            return false;
+        }
     }
 
     public LoggedInUser extractUserFromToken(String token) {
@@ -77,5 +88,13 @@ public class JwtUtil {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    void setSecret(String secret){
+        this.secret = secret;
+    }
+
+    int getExpirationTimeMillis() {
+        return expirationTimeMillis;
     }
 }
