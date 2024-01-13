dbd46cf4bdcf3ed00ab4435c96593250b347d2f5
Minjae-An <mj3242@naver.com>
2024-01-13 17:41:32 +0900

    feat(User) : UserImage 연관관계 정의 & 연관관계 편의 메서드 구현(#5)
    Cascade.ALL 옵션과 orphanRemoval=true 옵션을 함께 사용하여
    자식의 생명주기가 부모에 종속되도록 하였다.

--- src/main/java/com/example/springallinoneproject/user/entity/User.java
+++ src/main/java/com/example/springallinoneproject/user/entity/User.java
@@ -1,12 +1,16 @@
 package com.example.springallinoneproject.user.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,11 +31,25 @@ public class User {
     @Enumerated(value = EnumType.STRING)
     private SocialType socialType;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserImage> userImages;
+
     @Builder
     public User(String email, String username, String password, SocialType socialType) {
         this.email = email;
         this.username = username;
         this.password = password;
         this.socialType = socialType;
+        userImages = new ArrayList<>();
+    }
+
+    public void addUserImage(UserImage userImage) {
+        userImages.add(userImage);
+        userImage.updateUser(this);
+    }
+
+    public boolean deleteUserImage(String deleteImageFilename) {
+        return userImages.removeIf(userImage ->
+                userImage.getFilename().equals(deleteImageFilename));
     }
 }
