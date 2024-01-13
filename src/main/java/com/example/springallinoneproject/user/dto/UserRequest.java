7a68cba2b1d5ab9f2dde35c283f13d485698ba64
Minjae-An <mj3242@naver.com>
2024-01-13 18:12:06 +0900

    feat(UserRequest) : 단순 회원가입 DTO 구현

---
+++ src/main/java/com/example/springallinoneproject/user/dto/UserRequest.java
@@ -0,0 +1,18 @@
+package com.example.springallinoneproject.user.dto;
+
+import com.example.springallinoneproject.user.entity.SocialType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserRequest {
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class JoinRequest{
+        private String email;
+        private String username;
+        private String password;
+        private SocialType socialType;
+    }
+}
