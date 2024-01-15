039831a080e6c86affac326494d3148be6527dc6
Minjae-An <mj3242@naver.com>
2024-01-15 21:29:13 +0900

    feat(SpringAllinoneProjectApplication) : @EnableCaching 추가(#7)

--- src/main/java/com/example/springallinoneproject/SpringAllinoneProjectApplication.java
+++ src/main/java/com/example/springallinoneproject/SpringAllinoneProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.springallinoneproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringAllinoneProjectApplication {
 
 	public static void main(String[] args) {
