f194a2196c37b13ba16a82bfe364152292691e65
Minjae-An <mj3242@naver.com>
2024-01-17 13:30:09 +0900

    feat(TokenRepository) : TokenRepository 구현(#9)

---
+++ src/main/java/com/example/springallinoneproject/refresh_token/TokenRepository.java
@@ -0,0 +1,6 @@
+package com.example.springallinoneproject.refresh_token;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TokenRepository extends CrudRepository<Token, Long> {
+}
