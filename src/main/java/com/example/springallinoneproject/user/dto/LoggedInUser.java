65cc12e8c0864296f8bb04b2a1e363d9352d7a3f
Minjae-An <mj3242@naver.com>
2024-01-17 22:27:36 +0900

    feat(LoggedInUser) : Builder 사용하도록 로직 수정(#9)

--- src/main/java/com/example/springallinoneproject/user/dto/LoggedInUser.java
+++ src/main/java/com/example/springallinoneproject/user/dto/LoggedInUser.java
@@ -1,20 +1,18 @@
 package com.example.springallinoneproject.user.dto;
 
-import com.example.springallinoneproject.user.entity.User;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 @EqualsAndHashCode
 public class LoggedInUser {
     private String email;
     private String username;
 
-
-    public LoggedInUser(User user){
-        email = user.getEmail();
-        username = user.getUsername();
+    @Builder
+    private LoggedInUser(String email, String username) {
+        this.email = email;
+        this.username = username;
     }
 }
