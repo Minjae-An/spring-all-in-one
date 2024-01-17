83422bec0ca1a9d7030a853a94136364ef8b0573
Minjae-An <mj3242@naver.com>
2024-01-17 18:05:50 +0900

    feat(ErrorStatus) : USER_NOT_FOUND_BY_EMAIL, ALREADY_JOINED_USER 상태 코드 추가(#9)

--- src/main/java/com/example/springallinoneproject/api_payload/status_code/ErrorStatus.java
+++ src/main/java/com/example/springallinoneproject/api_payload/status_code/ErrorStatus.java
@@ -16,6 +16,8 @@ public enum ErrorStatus implements BaseCode{
 
     // user
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER4001", "해당 사용자가 존재하지 않습니다."),
+    _USER_NOT_FOUND_BY_EMAIL(HttpStatus.BAD_REQUEST, "USER4002", "해당 이메일의 사용자가 존재하지 않습니다."),
+    _ALREADY_JOINED_USER(HttpStatus.BAD_REQUEST, "USER4003", "해당 이메일로 이미 가입한 유저가 존재합니다."),
 
     // user image
     _USER_IMAGE_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_IMAGE4001", "해당 사용자 이미지가 존재하지 않습니다.");
