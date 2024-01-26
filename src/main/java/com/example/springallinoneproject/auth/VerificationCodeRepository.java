28a362bda7b4838d8b220c85780bb750cbc61b38
Minjae-An <mj3242@naver.com>
2024-01-26 13:18:25 +0900

    feat: 인증 코드 Repository 클래스 구현(#15) # ------------------------------------------------ # 위 줄은 제목 길이 제한을 위함 # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가

---
+++ src/main/java/com/example/springallinoneproject/auth/VerificationCodeRepository.java
@@ -0,0 +1,23 @@
+package com.example.springallinoneproject.auth;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class VerificationCodeRepository {
+    private final Map<String, VerificationCode> repository = new ConcurrentHashMap<>();
+
+    public VerificationCode save(VerificationCode verificationCode) {
+        return repository.put(verificationCode.getCode(), verificationCode);
+    }
+
+    public Optional<VerificationCode> findByCode(String code) {
+        return Optional.ofNullable(repository.get(code));
+    }
+
+    public void remove(VerificationCode verificationCode) {
+        repository.remove(verificationCode.getCode());
+    }
+}
